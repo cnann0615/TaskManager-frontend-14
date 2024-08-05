e6b1b9fc5be441c21932860e27adb7402c94f8bf
cnann0615 <leave.it.to.7010@gmail.com>
2024-08-06 00:00:55 +0900

    0805タスクのリンク修正

--- src/app/features/taskList/completedTaskList/CompletedTaskList.tsx
+++ src/app/features/taskList/completedTaskList/CompletedTaskList.tsx
@@ -98,14 +98,15 @@ const CompletedTaskList: React.FC = React.memo(() => {
             >
               ☑︎
             </button>
-            <div onClick={() => openTaskDetail(task)}>
-              <span className="cursor-pointer hover:bg-gray-100 flex-grow mx-2 line-through font-bold">
-                {task.title}
-              </span>
-              <span className="text-left w-32">
-                〆 {task.deadLine ? task.deadLine : "None"}
-              </span>
-            </div>
+            <span
+              onClick={() => openTaskDetail(task)}
+              className="cursor-pointer hover:bg-gray-100 flex-grow mx-2 line-through font-bold"
+            >
+              {task.title}
+            </span>
+            <span className="text-left w-32">
+              〆 {task.deadLine ? task.deadLine : "None"}
+            </span>
           </li>
         ))}
       </ul>
--- src/app/features/taskList/inCompletedTaskList/InCompletedTaskList.tsx
+++ src/app/features/taskList/inCompletedTaskList/InCompletedTaskList.tsx
@@ -102,14 +102,15 @@ const InCompletedTaskList: React.FC = React.memo(() => {
               >
                 ◻︎
               </button>
-              <div onClick={() => openTaskDetail(task)}>
-                <span className="cursor-pointer hover:bg-gray-100 flex-grow mx-2 font-bold">
-                  {task.title}
-                </span>
-                <span className="text-left w-32">
-                  〆 {task.deadLine ? task.deadLine : "None"}
-                </span>
-              </div>
+              <span
+                onClick={() => openTaskDetail(task)}
+                className="cursor-pointer hover:bg-gray-100 flex-grow mx-2 font-bold"
+              >
+                {task.title}
+              </span>
+              <span className="text-left w-32">
+                〆 {task.deadLine ? task.deadLine : "None"}
+              </span>
             </li>
           ))}
         </ul>
